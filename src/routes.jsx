a3f3968503b77fbf89b7f1a385d9cec45dee1b2a
Fiona <fionakanyi0@gmail.com>
2025-03-10 14:14:38 +0300

    first commit

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,29 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Directors from './pages/Directors';
+import Actors from './pages/Actors';
+import Movies from './pages/Movies';
+import ErrorPage from './pages/ErrorPage';
 
+const routes = createBrowserRouter([
+   {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+   },
+   {
+    path: '/directors',
+    element: <Directors />,
+   },
+   {
+    path: '/actors',
+    element: <Actors />,
+   },
+   {
+    path: '/movies/:id',
+    element: <Movies />,
+   },
 
-const routes = [
-   
-  ];
+  ]);
 
 export default routes;
